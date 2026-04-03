83b0da52f8d4b04824982f5af1605eb9506c75a0
MiltonJ23 <jonhmilton1991@gmail.com>
2026-04-03 18:23:44 +0100

    feat: testing the coordination of all of the components built in the milestone 2

--- cmd/kliops-api/main.go
+++ cmd/kliops-api/main.go
@@ -9,6 +9,11 @@ import (
 	"syscall"
 	"context"
 	"os"
+	"github.com/joho/godotenv"
+	"github.com/MiltonJ23/Kliops/internal/adapters/repositories"
+	"github.com/MiltonJ23/Kliops/internal/adapters/handlers"
+	"github.com/MiltonJ23/Kliops/internal/core/services"
+	"github.com/xuri/excelize/v2"
 )
 
 
@@ -19,6 +24,47 @@ type HealthResponse struct {
 }
 
 func main(){
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning : file .env not found ")
+	}
+
+	log.Println("--> Initializing Kliops API Gateway ....")
+
+
+
+	minioStorage, err := repositories.NewMinioStorage("localhost:9000",os.Getenv("MINIO_ROOT_USER"),os.Getenv("MINIO_ROOT_PASSWORD"),false)
+	if err != nil {
+		log.Fatalf("unable to reach miniO : %v",err)
+	}
+
+	pricingService := services.NewPricingService() 
+	if _, err := os.Stat("dummy_prices.xlsx"); os.IsNotExist(err) {
+		log.Println("Création du fichier Excel de test...")
+		f := excelize.NewFile()
+		// excelize crée une feuille "Sheet1" par défaut, on la renomme en "Prix" comme attendu par l'adapter
+		f.SetSheetName("Sheet1", "Prix")
+		f.SetCellValue("Prix", "A1", "ART01")
+		f.SetCellValue("Prix", "B1", 150.50) // Prix du ART01
+		f.SetCellValue("Prix", "A2", "ART02")
+		f.SetCellValue("Prix", "B2", 45.00)
+		
+		if err := f.SaveAs("dummy_prices.xlsx"); err != nil {
+			log.Fatalf("Impossible de sauvegarder l'Excel de test : %v", err)
+		}
+		f.Close()
+	}
+
+	excelStrategy := repositories.NewExcelPricing("dummy_prices.xlsx")
+	erpStrategy := repositories.NewERPPricing("http://api.erp-btp.local") 
+
+	pricingService.RegisterStrategy("excel",excelStrategy)
+	pricingService.RegisterStrategy("erp",erpStrategy)
+
+	// we then initialize the gateway 
+	gatewayHandler := handlers.NewGatewayHandler(minioStorage,pricingService)
+
+
 	mux := http.NewServeMux() // Initializing the router 
 
 	//let's add the route to ensure that the API is alive 
@@ -30,6 +76,13 @@ func main(){
 			Message: "Kliops API Gatewau is running ",
 		})
 	})
+
+	// let's prepare the other routes of the API, the ones protected by the Middleware 
+	apiMux := http.NewServeMux()
+	apiMux.HandleFunc("POST /api/v1/upload",gatewayHandler.HandleUpload)
+	apiMux.HandleFunc("GET /api/v1/price",gatewayHandler.HandlePrice)
+
+	mux.Handle("/api/v1/", handlers.APIKeyMiddleware(apiMux)) 
 
 	// let's configure the server 
 	srv := &http.Server{
