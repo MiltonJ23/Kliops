ca2d2a0cd1b4403c405756cbeae99bf940860b2c
MiltonJ23 <jonhmilton1991@gmail.com>
2026-04-05 14:19:12 +0100

    fix: renamed the handler declaration in the ConsumeJob method definition

--- internal/core/ports/ingestion.go
+++ internal/core/ports/ingestion.go
@@ -1,24 +1,24 @@
-package ports 
+package ports
 
 import (
 	"context"
 )
 
-type JobStatus string 
+type JobStatus string
 
 const (
-	JobPending JobStatus = "PENDING"
+	JobPending    JobStatus = "PENDING"
 	JobProcessing JobStatus = "PROCESSING"
-	JobCompleted JobStatus = "COMPLETED"
-	JobFailed JobStatus = "FAILED"
+	JobCompleted  JobStatus = "COMPLETED"
+	JobFailed     JobStatus = "FAILED"
 )
 
 type ProjectManifest struct {
-	ExternalID string 
-	Titre 	   string 
-	Client 	   string 
-	Status     string 
-	FichierDCE string 
+	ExternalID string
+	Titre      string
+	Client     string
+	Status     string
+	FichierDCE string
 	FichierMEM string
 }
 
@@ -28,22 +28,21 @@ type ExtractedPair struct {
 }
 
 type IngestionRepository interface {
-	// ExecuteTx is the transactionality warrant 
-	ExecuteTx(ctx context.Context, fn func(txRepo IngestionRepository) error) error 
-	CheckProjectExists(ctx context.Context, externalID string) (bool,error)
-	CreateProject(ctx context.Context, p ProjectManifest) (string,error)
-	SaveDocument(ctx context.Context, projectId,docType,minioPath string) error 
-	CreateJob(ctx context.Context,projectID string) error
-	UpdateJobStatus(ctx context.Context,jobId string,status JobStatus,errMsg string) error 
-	SaveReponseHistorique(ctx context.Context, projectID,exigence,reponse,qdrantID string) error
+	// ExecuteTx is the transactionality warrant
+	ExecuteTx(ctx context.Context, fn func(txRepo IngestionRepository) error) error
+	CheckProjectExists(ctx context.Context, externalID string) (bool, error)
+	CreateProject(ctx context.Context, p ProjectManifest) (string, error)
+	SaveDocument(ctx context.Context, projectId, docType, minioPath string) error
+	CreateJob(ctx context.Context, projectID string) error
+	UpdateJobStatus(ctx context.Context, jobId string, status JobStatus, errMsg string) error
+	SaveReponseHistorique(ctx context.Context, projectID, exigence, reponse, qdrantID string) error
 }
 
-
 type MessageQueue interface {
-	PublishJob(ctx context.Context,jobID,projectID string) error 
-	ConsumeJob(ctx context.Context, hanlder func(ctx context.Context,jobID,projectID string) error) error
+	PublishJob(ctx context.Context, jobID, projectID string) error
+	ConsumeJob(ctx context.Context, handler func(ctx context.Context, jobID, projectID string) error) error
 }
 
 type LLMExtractor interface {
-	ExtractRequirementsAndAnswers(ctx context.Context,dceText,memoireText string) ([]ExtractedPair,error)
+	ExtractRequirementsAndAnswers(ctx context.Context, dceText, memoireText string) ([]ExtractedPair, error)
 }
