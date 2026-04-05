3fba46c726557f08b227ed381ec03d882fc5e0a4
MiltonJ23 <jonhmilton1991@gmail.com>
2026-04-05 10:18:16 +0100

    fix: fixed the formating here

--- internal/core/domain/tender.go
+++ internal/core/domain/tender.go
@@ -1,26 +1,24 @@
 package domain
 
-
 import "time"
 
-
-// AppelOffre refers to the complete consulation file (DCE)
+// AppelOffre refers to the complete consultation file (DCE)
 type AppelOffre struct {
-	ID string 
-	Titre string 
-	MaitreDouvrage string 
-	DateLimite time.Time 
-	RegelementConsultation string 
-	CCTP string // Specifications of Special Technical Clauses 
-	CCAP string // Specifications of Special Administrative Clauses
-	BPU_DPGF string // Price Schedule or Price Breakdown 
+	ID                     string
+	Titre                  string
+	MaitreDouvrage         string
+	DateLimite             time.Time
+	RegelementConsultation string
+	CCTP                   string // Specifications of Special Technical Clauses
+	ReglementConsultation  string
+	BPU_DPGF               string // Price Schedule or Price Breakdown
 }
 
 type ReponseHistorique struct {
-	ID string 
-	AppelOffreID string 
-	ExigenceTechnique string // abstract of the CCTP related to the AppelOffre it was suppose to answer linked through AppelOffreID 
-	ReponseApportee string // Technical Brief which was written to respond to the call for tenders
-	PrixPropose float64 
-	Gagne bool 
+	ID                string
+	AppelOffreID      string
+	ExigenceTechnique string // abstract of the CCTP related to the AppelOffre it was suppose to answer linked through AppelOffreID
+	ReponseApportee   string // Technical Brief which was written to respond to the call for tenders
+	PrixPropose       float64
+	Gagne             bool
 }
